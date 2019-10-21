87db50a2069da75c77545c8d183b89552bbaba33
Ram <ramdevukd@gmail.com>
2019-10-21 22:46:50 +0530

    fixed some bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,12 @@ class UsersController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         unless user
-            newUser = User.new(name: params[:name], email: params[:email], access_token: params[:access_token])
-            newUser.save
+            user = User.new(name: params[:name], email: params[:email], access_token: params[:access_token])
+            user.save
         end
+
         # Implement JWT
-        render json: {'name' => newUser.name, 'email' => newUser.email}
+        render json: {'name' => user.name, 'email' => user.email}
     end
 
 end
